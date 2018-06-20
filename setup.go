d9b51c389e9bd7d28bdbb25078fb1437d0af6929
KappaBull <kappa8v11@gmail.com>
2018-06-21 03:17:39 +0900

    Add Gravatar、GoogleFont

--- setup.go
+++ setup.go
@@ -1,10 +1,19 @@
 package bootstrapquickstart
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
+)
+
+const (
+	myname  = "KappaBull"
+	mail    = "kappa8v11@gmail.com"
+	twitter = myname
+	qiita   = myname
 )
 
 var templates = make(map[string]*template.Template)
@@ -30,11 +39,22 @@ func initializeTemplates() {
 }
 
 type Welcome struct {
-	Title   string
-	Message string
+	Title       string
+	Message     string
+	GravatarURL string
 }
 
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
-	message := Welcome{Title: "KappaBull", Message: "Hosting to GAE/go"}
+	message := Welcome{
+		Title:       myname,
+		Message:     "Hosting to GAE/go",
+		GravatarURL: _GetGravatarURL("128"),
+	}
 	templates["welcome.html"].ExecuteTemplate(w, "outerTheme", &message)
+}
+
+func _GetGravatarURL(size string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(mail))
+	return "http://www.gravatar.com/avatar/" + hex.EncodeToString(hasher.Sum(nil)) + ".jpg?s=" + size
 }
